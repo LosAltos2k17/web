fa06a87e7ca5c148131be0684fa45d37d4f7764b
Nihal George <nihaleg@gmail.com>
2017-02-05 08:42:13 +0000

    nihal and ananth in this bitch

--- index.js
+++ index.js
@@ -4,36 +4,85 @@ var path = require('path');
 var fs = require('fs');
 var randomstring = require('randomstring');
 var Clarifai = require('clarifai');
+var firebase = require('firebase');
+
+var NutritionixClient = require('nutritionix');
 
 var app = express();
+app.use(express.static('public'));
 
 var clarifapp = new Clarifai.App(
   'Ee3tsKiM81juIGCz9nVVjjpmfNjARfMTVk0hQVN0',
   '0D9PSEo4vaZEDS1nsThlTrtiwbtHLuF1_wChyCpp'
 );
 
-app.get('/', function(req, res) {  
+var nutritionix = new NutritionixClient({
+  appId: 'a205c9e0',
+  appKey: 'aa0a60ab51a8792de0fa1321657fca80'
+});
+
+firebase.initializeApp({
+  databaseURL: "https://glucagon-b50f6.firebaseio.com/",
+});
+
+
+app.get('/', function(req, res) {
   console.log("test");
   res.render(path.join(__dirname, 'views', 'dash.ejs'));
 });
 
-app.get('/login', function(req, res) {  
+app.get('/login', function(req, res) {
   console.log("test");
   res.render(path.join(__dirname, 'views', 'login.ejs'));
 });
 
 
-app.get('/image/:fileName', function (req, res) {
-  res.sendFile(__dirname+'/images/' + req.params.fileName);
-})
+app.get('/image/:fileName', function(req, res) {
+  //res.sendFile(__dirname+'/images/' + req.params.fileName);
+  res.sendFile(__dirname + '/pizza.png');
+});
 
-app.post('/pic', function(req,res){
-    var decodedImage = new Buffer(req.query.pic, 'base64');
-    var fileName = randomstring.generate();
-    fs.writeFile('./images/' + fileName + '.jpg', decodedImage, function(err) {if(err) console.log(err)});
-    clarifapp.models.predict(Clarifai.GENERAL_MODEL, 'https://los-altos-hacks-nihaleg.c9users.io/image/' + fileName).then(
+app.post('/pic', function(req, res) {
+  var decodedImage = new Buffer(req.query.pic, 'base64');
+  var fileName = randomstring.generate();
+  fs.writeFile('./images/' + fileName + '.jpg', decodedImage, function(err) {
+    if (err) console.log(err);
+  });
+  clarifapp.models.predict(Clarifai.GENERAL_MODEL, 'https://los-altos-hacks-2-nihaleg.c9users.io/image/' + fileName).then(
     function(response) {
-      console.log(response.rawData.outputs[0].dat);
+      var food = response.rawData.outputs[0].data.concepts[0].name;
+      nutritionix.search({
+          q: food,
+          // use these for paging
+          limit: 10,
+          offset: 0,
+          // controls the basic nutrient returned in search
+          search_nutrient: 'calories',
+        }).then(function(searchResults) {
+          var itemCode = searchResults.results[0].resource_id;
+          nutritionix.item({
+              id: itemCode
+            })
+            .then(function(searchResults) {
+                console.log(searchResults.label.nutrients.filter(function (element) {//firebase.database().ref('/nihalstuff/').push
+                  return element.name == "Sugars" || element.name == "Calories" || element.name == "Cholesterol" || element.name == "Protein"|| element.name == "Sodium";
+                }));
+              },
+              function(err) {
+                console.log(err);
+              })
+            .catch(function(err) {
+              console.log(err);
+            });
+        }, function(err) {
+          console.log(err);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+
+
+
     },
     function(err) {
       console.error(err);
@@ -42,7 +91,7 @@ app.post('/pic', function(req,res){
 });
 
 app.listen(process.env.PORT, function(err) {
-  if(err) {
+  if (err) {
     return console.error(err);
   }
   console.log('app listening on ' + config.PORT);
